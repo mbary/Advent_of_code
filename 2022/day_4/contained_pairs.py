5e7cd3719619b8c2717ecc1e535f7d9da9908f6b
mbary <michal.barrington@gmail.com>
2022-12-14 18:05:40 +0100

    day_4 part2 solution

--- 2022/day_4/contained_pairs.py
+++ 2022/day_4/contained_pairs.py
@@ -23,7 +23,7 @@ pairs = [get_pairs(x) for x in data]
 print(pairs[0])
     
 
-def check_pair(x):
+def check_full_overlap(x):
 
     p1,p2 = x[0],x[1]
 
@@ -35,7 +35,20 @@ def check_pair(x):
     
     else: return 0
 
-overlapping = [check_pair(x) for x in pairs]
+overlapping = [check_full_overlap(x) for x in pairs]
 # print(overlapping)
 print(sum(overlapping))
+
+def check_partial_overlap(x):
+
+    p1,p2 = x[0],x[1]
+    range_1 = set(range(p1[0],p1[1]+1))
+    range_2 = set(range(p2[0],p2[1]+1))
+    print(range_1)
+    print(range_2)
+    return [1 if range_1.intersection(range_2) else 0][0]
     
+
+partially_overlapping = [check_partial_overlap(x) for x in pairs]    
+print(sum(partially_overlapping))
+# print(partially_overlapping)
