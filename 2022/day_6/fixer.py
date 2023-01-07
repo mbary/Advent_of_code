fb258b528f47fe1acbdf37499d8ad9d1bffa707d
mbary <michal.barrington@gmail.com>
2023-01-07 11:53:24 +0100

    part2 solution - again, fairly ugly annd I see some logical inconsistencies but baby I'm on FIRE

--- 2022/day_6/fixer.py
+++ 2022/day_6/fixer.py
@@ -37,6 +37,23 @@ def get_packet(stream: Generator) -> List:
         
     return start_index
 
+
+def get_message(stream: Generator) -> List:
+    packet = list()
+    packet_set = set()
+    start_index = 0
+    while len(packet_set) < 14:
+        packet.append(pass_data(stream))
+        start_index += 1
+        print(packet)
+        if len(packet) == 14:
+            packet_set = set(packet)
+            print(packet_set)
+            if len(packet_set) <15:
+                packet.pop(0)
+        
+    return start_index
+
 # indecies = []
 
 # try:
@@ -45,4 +62,5 @@ def get_packet(stream: Generator) -> List:
 # except:
 #     pass
 # print(indecies)
-get_packet(data_gen)
+# get_packet(data_gen)
+get_message(data_gen)
